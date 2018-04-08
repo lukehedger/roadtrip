1cc384950209ef7eef28531ffe39cdd812f7a50a
lukehedger <luke.hedger@gmail.com>
2018-04-08 19:09:40 +0100

    Update theme

--- app/core/style/theme.js
+++ app/core/style/theme.js
@@ -7,11 +7,13 @@
  */
 const customTheme = {
   palette: {
+    blue: '#3C8DE3',
     green: '#4E9172',
     grey: '#767C91',
     orange: '#E57453',
     peach: '#F9CDB1',
     primary: '#000000',
+    red: '#D14545',
     yellow: '#F0E9D8',
   },
 }
